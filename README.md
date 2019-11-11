119e1a9b9b6c4d607c805400bcd27340ead8df1a
Anders Magnusson <restiyardwen@gmail.com>
2019-11-11 13:18:37 +0100

    Modify readme, comment gulpfile and adjust package.json

--- README.md
+++ README.md
@@ -25,9 +25,13 @@ npm start
 * `dist` - The converted files that are linked via `index.html`. You don't have to touch these files
     - `js` - All converted `js`-files go here
     - `css` - All converted `css`-files go here
-* `src` - This is where your development-files are. There are the ones you work on
+    - `partials` - All converted `pug`-files from `src/pug/partials` go here
+    - `index.html` - rendered version of `src/pug/index.pug`
+* `src` - This is where your development-files are. These are the ones you work on
     - `js` - All unconverted `js`-files
     - `scss` - All unconverted `scss`-files
+    - `pug` - Should only hold `index.pug` and `partials`.
+    - `pug/partials` - All uncoverted `pug partials`.
 * `Gulpfile.js` - This config file must be in the root-folder
 * `index.html` - The index must be in the root-folder
 * `package.json` - All the dependencies and config for the project
@@ -35,15 +39,19 @@ npm start
 
 ## Dependencies used in this boilerplate
 
-* [gulp](https://www.npmjs.com/package/gulp)
-* [gulp-sass](https://www.npmjs.com/package/gulp-sass)
-* [gulp-rename](https://www.npmjs.com/package/gulp-rename)
-* [browser-sync](https://www.npmjs.com/package/browser-sync)
-* [gulp-postcss](https://www.npmjs.com/package/gulp-postcss)
-* [autoprefixer](https://www.npmjs.com/package/autoprefixer)
-* [gulp-babel](https://www.npmjs.com/package/gulp-babel)
-* [babel-preset-env](https://www.npmjs.com/package/babel-preset-env)
-* [browserify](https://www.npmjs.com/package/browserify)
-* [babelify](https://www.npmjs.com/package/babelify)
-* [gulp-util](https://www.npmjs.com/package/gulp-util)
-* [vinyl-source-stream](https://www.npmjs.com/package/vinyl-source-stream)
+* ["browserify"](https://www.npms.com/package/browserify)
+* ["gulp"](https://www.npms.com/package/gulp)
+* ["gulp-babel"](https://www.npms.com/package/gulp-babel)
+* ["babelify"](https://www.npms.com/package/babelify)
+* ["@babel/core"](https://www.npms.com/package/@babel/core)
+* ["@babel/preset-env"](https://www.npms.com/package/@babel/preset-env)
+* ["gulp-sass"](https://www.npms.com/package/gulp-sass)
+* ["gulp-postcss"](https://www.npms.com/package/gulp-postcss)
+* ["autoprefixer"](https://www.npms.com/package/autoprefixer)
+* ["cssnano"](https://www.npms.com/package/cssnano)
+* ["gulp-pug"](https://www.npms.com/package/gulp-pug)
+* ["gulp-rename"](https://www.npms.com/package/gulp-rename)
+* ["gulp-util"](https://www.npms.com/package/gulp-util)
+* ["browser-sync"](https://www.npms.com/package/browser-sync)
+* ["rimraf"](https://www.npms.com/package/rimraf)
+* ["vinyl-source-stream"](https://www.npms.com/package/vinyl-source-stream)
--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,87 @@
 //utils
 const 
-    gulp = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    gutil = require('gulp-util'),
-    rename = require('gulp-rename');
+    gulp = require( 'gulp' ),
+    browserSync = require( 'browser-sync' ).create(),
+    gutil = require( 'gulp-util' ),
+    rename = require( 'gulp-rename' );
 
-//css
+// sass
 const
-    sass = require('gulp-sass'),
-    postcss = require('gulp-postcss');
+    sass = require( 'gulp-sass' );
 
-//postcss
+// postcss
 const 
-    cssnano = require('cssnano'),
-    autoprefixer = require('autoprefixer'),
-    csso = require('postcss-csso');
+    postcss = require( 'gulp-postcss' ),
+    cssnano = require( 'cssnano' ),
+    autoprefixer = require( 'autoprefixer' ),
+    csso = require( 'postcss-csso' );
 
 
-//babel + browserify
+// babel + browserify
 const
-    babel = require('gulp-babel'),
-    browserify = require('browserify'),
-    babelify = require('babelify'),
-    source = require('vinyl-source-stream');
+    babel = require( 'gulp-babel' ),
+    browserify = require( 'browserify' ),
+    babelify = require( 'babelify' ),
+    source = require( 'vinyl-source-stream' );
 
-//pug
+// pug
 const
-    pug = require('gulp-pug');
+    pug = require( 'gulp-pug' );
 
+/**
+ * Default task.
+ * 
+ * Watches source folders for changes that triggers the appropriate task.
+ * Watches dist folders for changes that triggers browser reload.
+ */
 gulp.task('default', () => {
     browserSync.init({
         server: {
             baseDir: "./dist/"
         }
-    });
-    gulp.watch('./dist/html/*.html').on('change', browserSync.reload);
-    gulp.watch('./src/scss/**/*.scss', gulp.task('sass') );
-    gulp.watch('./src/js/*.js', gulp.task('babel'));
-    gulp.watch('./src/pug/*.pug', gulp.task('pug'));
-    gulp.watch('./src/pug/**/*.pug', gulp.task('pug'));
-});
+    } );
+    gulp.watch( './dist/**/*' ).on('change', browserSync.reload );
+    gulp.watch('./src/scss/**/*.scss', gulp.task( 'sass' ) );
+    gulp.watch('./src/js/*.js', gulp.task( 'babel' ) );
+    gulp.watch('./src/pug/**/*.pug', gulp.task( 'pug' ) );
+} );
 
+// Rebuilds css from sass (with some postcss).
 gulp.task('sass', () => {
     const plugins = [
         autoprefixer( { browserlist: [ 'last 2 versions' ] } ),
         cssnano(),
         csso()
     ];
-    return gulp.src('./src/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(plugins))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist/css'))
-        .pipe(browserSync.stream());
-});
+    return gulp.src( './src/scss/**/*.scss' )
+        .pipe( sass().on('error', sass.logError ) )
+        .pipe( postcss(plugins ) )
+        .pipe(rename({suffix: '.min'} ) )
+        .pipe( gulp.dest('./dist/css' ) )
+        .pipe( browserSync.stream() );
+} );
 
-gulp.task('babel', function() {
+// Transforms javascript with babel and @babel/preset-env
+gulp.task('babel', () => {
     return browserify({
         entries: './src/js/main.js',
         debug: true
-    })
-    .transform(babelify, { presets: ['@babel/preset-env'] })
-    .on('error',gutil.log)
+    } )
+    .transform(babelify, { presets: ['@babel/preset-env'] } )
+    .on( 'error',gutil.log )
     .bundle()
-    .on('error',gutil.log)
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist/js'))
-    .pipe(browserSync.stream());
-});
+    .on( 'error',gutil.log )
+    .pipe( source('bundle.js' ) )
+    .pipe( gulp.dest('dist/js' ) )
+    .pipe( browserSync.stream() );
+} );
 
-gulp.task('pug', function() {
-    return gulp.src('./src/pug/**/*.pug')
+// Transforms pugs to html
+gulp.task('pug', () => {
+    return gulp.src( './src/pug/**/*.pug' )
     .pipe(pug({
         doctype: 'html',
         pretty: true,
-    }))
-    .pipe(gulp.dest('./dist/'));
-});
+    } ) )
+    .pipe( gulp.dest('./dist/' ) );
+} );
